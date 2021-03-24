6e9acb6514cfed4d89e24d4e9bdf939afcf20228
liuchunyuan <liuchunyuan@haier.com>
2021-03-24 09:05:40 +0800

    add test class

--- test_mubu_login.py
+++ test_mubu_login.py
@@ -1,57 +1,57 @@
 import requests
 
+class TestMubuLogin:
+    def test_get_home_page(self):
+        url = "https://mubu.com/"
+        headers = {
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
+        }
+        resp = requests.get(url, headers=headers, verify=False)
+        # print("resp---", resp.text)
 
-def test_get_home_page():
-    url = "https://mubu.com/"
-    headers = {
-    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
-    }
-    resp = requests.get(url, headers=headers, verify=False)
-    # print("resp---", resp.text)
-
-    assert resp.status_code == 200
-
-
-def test_login():
-    url = "https://mubu.com/login"
-    headers = {
-    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
-    }
-    resp = requests.get(url, headers=headers, verify=False)
-    # print("resp---", resp.text)
-
-    assert resp.status_code == 200
+        assert resp.status_code == 200
 
 
-def test_login_password():
-    url = "https://mubu.com/login/password"
-    headers = {
-    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
-    }
-    resp = requests.get(url, headers=headers, verify=False)
-    # print("resp---", resp.text)
+    def test_login(self):
+        url = "https://mubu.com/login"
+        headers = {
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
+        }
+        resp = requests.get(url, headers=headers, verify=False)
+        # print("resp---", resp.text)
 
-    assert resp.status_code == 200
+        assert resp.status_code == 200
 
 
-def test_login_post():
-    url = "https://mubu.com/api/login/submit"
-    headers = {
-    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
-    "content-type":"application/x-www-form-urlencoded; charset=UTF-8"
-    }
-    data = "phone=17601007087&password=cyliu836&remember=true"
+    def test_login_password(self):
+        url = "https://mubu.com/login/password"
+        headers = {
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36"
+        }
+        resp = requests.get(url, headers=headers, verify=False)
+        # print("resp---", resp.text)
 
-    # # data = {
-    #     "phone": "17601007087",
-    #     "password": "cyliu836",
-    #     "remember": True
-    # }
-    resp = requests.post(url, headers=headers, data=data, verify=False)
-    print("resp---", resp.text)
+        assert resp.status_code == 200
 
-    assert resp.status_code == 200
-    resp_json = resp.json()
-    assert resp_json["code"] == 0
+
+    def test_login_post(self):
+        url = "https://mubu.com/api/login/submit"
+        headers = {
+        "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36",
+        "content-type":"application/x-www-form-urlencoded; charset=UTF-8"
+        }
+        data = "phone=17601007087&password=cyliu836&remember=true"
+
+        # # data = {
+        #     "phone": "17601007087",
+        #     "password": "cyliu836",
+        #     "remember": True
+        # }
+        resp = requests.post(url, headers=headers, data=data, verify=False)
+        print("resp---", resp.text)
+
+        assert resp.status_code == 200
+        resp_json = resp.json()
+        assert resp_json["code"] == 0
 
 
